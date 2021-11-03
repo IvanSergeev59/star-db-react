b760c8d7f0e3ba953dcf111834e3a84f019637af
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-11-03 17:49:25 +0500

    add propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -7,10 +8,14 @@ import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
-  static defaultProps = {
-    updateInterval: 2000
-  }
   
+  static defaultProps = {
+    updateInterval: 10000
+  }
+
+  static propTypes = {
+    updateInterval: PropTypes.number
+  }
   swapiService = new SwapiService();
 
   state = {
